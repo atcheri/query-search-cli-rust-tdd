7cd8a31e4d702a724f54041d3976ac5e3e59ba63
Atsuhiro <atcheri@gmail.com>
2025-04-02 12:08:46 +0200

    test: implement the search fn
    Red + Green phase

--- tests/lib.rs
+++ tests/lib.rs
@@ -2,16 +2,21 @@ use core::str;
 
 #[test]
 fn should_return_a_single_line_when_matched_with_single_line() {
+    // arrange
     let query = "TDD";
     let content = "TDD rocks";
 
+    // act
     let lines = QuerySearcher::search(&query, &content);
+
+    // assert
+    assert_eq!(lines, content)
 }
 
 pub struct QuerySearcher {}
 
 impl QuerySearcher {
     fn search(query: &str, content: &str) -> String {
-        String::from("")
+        String::from("TDD rocks")
     }
 }
