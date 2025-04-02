f3de46a01a5dcecb663b15af5a3287eb21fbcad9
Atsuhiro <atcheri@gmail.com>
2025-04-02 12:50:04 +0200

    feat: split the text into lines and return when found
    Green phase

--- tests/lib.rs
+++ tests/lib.rs
@@ -41,14 +41,29 @@ fn should_return_no_line_when_matched_with_none() {
     assert!(lines.is_empty());
 }
 
+#[test]
+fn should_return_a_single_line_when_matched_with_second_line() {
+    // arrange
+    let query = "TDD";
+    let content = "First line without the word\nTDD rocks";
+
+    // act
+    let lines = QuerySearcher::search(&query, &content);
+
+    // assert
+    assert_eq!(lines, "TDD rocks");
+}
+
 pub struct QuerySearcher {}
 
 impl QuerySearcher {
     fn search(query: &str, content: &str) -> String {
-        if !content.contains(query) {
-            return String::from("");
+        for line in content.lines() {
+            if line.contains(query) {
+                return String::from(line);
+            }
         }
 
-        String::from(content)
+        String::from("")
     }
 }
