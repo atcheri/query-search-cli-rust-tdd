3b9162681331aa23bb29966444d9fc08eae865f8
Atsuhiro <atcheri@gmail.com>
2025-04-03 08:55:30 +0200

    test: add multiple match scenario
    Red phase

--- tests/main.rs
+++ tests/main.rs
@@ -9,3 +9,17 @@ fn should_print_a_single_line_when_matche_single_line() {
         .assert()
         .stdout(predicates::str::contains("4: Savor the simple joys for a joyful life."));
 }
+
+#[test]
+fn should_print_multiple_lines_when_matche_with_multiple() {
+    let mut cmd = Command::cargo_bin("cli").unwrap();
+
+    cmd.arg("happiness")
+        .arg("./tests/happiness.txt")
+        .assert()
+        .stdout(
+            predicates::str::contains(
+                "2: Happiness is found in the little things.\n5: In simplicity, we discover true happiness."
+            )
+        );
+}
