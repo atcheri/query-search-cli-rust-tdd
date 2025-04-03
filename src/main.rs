807b6b22758bf09d3be6537f9b44730f797bb93e
Atsuhiro <atcheri@gmail.com>
2025-04-03 09:08:30 +0200

    feat: add logic to exit progam when no file path argument is specified
    Green phase

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,10 @@ impl FileReader for SystemFileReader {
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(args);
+    let config = Config::new(args).unwrap_or_else(|err| {
+        eprintln!("Parsing arguments error: {}", err);
+        std::process::exit(1);
+    });
 
     let lines = QuerySearcher::new(SystemFileReader).search(&config.query, &config.file_path);
 
@@ -25,10 +28,14 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: Vec<String>) -> Self {
+    pub fn new(args: Vec<String>) -> Result<Config, String> {
+        if args.len() < 3 {
+            return Err("not enough arguments specified".to_string());
+        }
+
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Config { query: query, file_path: file_path }
+        Ok(Config { query: query, file_path: file_path })
     }
 }
